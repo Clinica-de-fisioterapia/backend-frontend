3c543b295d060f74fbdbb6d91b324df33e7f47b3
luisgabrielMR <luisgabrielmr2019@gmail.com>
2025-09-22 19:42:37 -0300

    Criação da estrutura backend e movidos arquivos do front somente para o front

---
+++ Backend/tests/Chronosystem.Api.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Chronosystem.Api.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
