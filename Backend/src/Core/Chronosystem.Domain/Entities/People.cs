62851a981a6a1aec52e9fb41fc376ee9d2023b3d
xLeotte <victorleottegaz@gmail.com>
2025-11-20 15:23:28 -0300

    banco acesso
    banco acesso

--- Backend/src/Core/Chronosystem.Domain/Entities/People.cs
+++ Backend/src/Core/Chronosystem.Domain/Entities/People.cs
@@ -2,30 +2,33 @@ using System;
 
 namespace Chronosystem.Domain.Entities
 {
-    public class Person
-    {
-    private Guid actorUserId;
-
+public class Person
+{
+    public Person() {} 
     public Person(string fullName, string? cpf, string? phone, string? email, Guid actorUserId)
     {
-      FullName = fullName;
-      Cpf = cpf;
-      Phone = phone;
-      Email = email;
-      this.actorUserId = actorUserId;
+        FullName = fullName;
+        Cpf = cpf;
+        Phone = phone;
+        Email = email;
+        ActorUserId = actorUserId;
     }
 
     public Guid Id { get; set; }
-        public string FullName { get; set; } = default!;
-        public string? Cpf { get; set; }
-        public string? Phone { get; set; }
-        public string? Email { get; set; }
+    public string FullName { get; set; } = default!;
+    public string? Cpf { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
 
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public long RowVersion { get; set; }
-        public Guid? CreatedBy { get; set; }
-        public Guid? UpdatedBy { get; set; }
-        public DateTime? DeletedAt { get; set; }
-    }
+    public Guid? CreatedBy { get; set; }
+    public Guid? UpdatedBy { get; set; }
+    public Guid ActorUserId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public long RowVersion { get; set; }
+    public DateTime? DeletedAt { get; set; }
+}
+
+
 }
